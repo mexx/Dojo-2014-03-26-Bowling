772a6c31f3beb73f3dbed35daa0d92420d9caa3f
Max Malook <community@malook.de>
2014-03-26 19:52:27 +0100

    First spare

--- Bowling.fs
+++ Bowling.fs
@@ -3,16 +3,24 @@
 open Xunit
 open FsUnit.Xunit
 
-let calculate input =
-    let score c =
-        if (c = '-') then
-            0
-        else
-            System.Int32.Parse(string c)
+let calculate (input: string) =
+    let rec calculate (input: char list) =
+        let score c =
+            if (c = '-') then
+                0
+            elif (c = '/') then
+                0
+            else
+                System.Int32.Parse(string c)
+
+        match input with
+        | c :: '/' :: rest -> 10 + calculate rest
+        | c :: rest -> score c + calculate rest
+        | [] -> 0
 
     input
-    |> Seq.map score
-    |> Seq.sum
+    |> List.ofSeq
+    |> calculate
 
 [<Fact>]
 let ``Gutter game scores zero`` ()=
@@ -25,3 +33,15 @@ let ``Single throw game scores four`` ()=
     "4-------------------"
     |> calculate
     |> should equal 4
+
+[<Fact>]
+let ``Single throw at end game scores four`` ()=
+    "------------------5-"
+    |> calculate
+    |> should equal 5
+    
+[<Fact>]
+let ``Spare and gutter game scores four`` ()=
+    "4/------------------"
+    |> calculate
+    |> should equal 10
