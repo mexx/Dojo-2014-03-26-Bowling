eae364ff3a9f8e8a6175a77a4e631f7943d6981a
Max Malook <community@malook.de>
2014-03-26 19:36:32 +0100

    Single throw

--- Bowling.fs
+++ Bowling.fs
@@ -4,10 +4,24 @@ open Xunit
 open FsUnit.Xunit
 
 let calculate input =
-    0
+    let score c =
+        if (c = '-') then
+            0
+        else
+            System.Int32.Parse(string c)
+
+    input
+    |> Seq.map score
+    |> Seq.sum
 
 [<Fact>]
 let ``Gutter game scores zero`` ()=
     "--------------------"
     |> calculate
     |> should equal 0
+
+[<Fact>]
+let ``Single throw game scores four`` ()=
+    "4-------------------"
+    |> calculate
+    |> should equal 4
