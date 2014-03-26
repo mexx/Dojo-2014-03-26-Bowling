fafc9191fb2c972a2b95f3a27d55891a84d0b834
Max Malook <community@malook.de>
2014-03-26 20:03:42 +0100

    First strike

--- Bowling.fs
+++ Bowling.fs
@@ -8,14 +8,13 @@ let calculate (input: string) =
         let score c =
             if (c = '-') then
                 0
-            elif (c = '/') then
-                0
             else
                 System.Int32.Parse(string c)
 
         match input with
         | c :: '/' :: next :: [] -> 10 + score next
         | c :: '/' :: next :: rest -> 10 + score next + calculate (next :: rest)
+        | 'X' :: rest -> 10 + calculate rest
         | c :: rest -> score c + calculate rest
         | [] -> 0
 
@@ -58,3 +57,10 @@ let ``Spare and following throw at end of gutter game scores thirteen`` ()=
     "-----------------4/3"
     |> calculate
     |> should equal 13
+
+[<Fact>]
+let ``Strike and gutter game scores ten`` ()=
+    "X------------------"
+    |> calculate
+    |> should equal 10
+
