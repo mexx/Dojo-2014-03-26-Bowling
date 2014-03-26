b28a8eea3a6d124306e24a68a93560387215287e
Max Malook <community@malook.de>
2014-03-26 19:30:46 +0100

    Gutter game

---
+++ Bowling.fs
@@ -0,0 +1,13 @@
+﻿module FsBowling
+
+open Xunit
+open FsUnit.Xunit
+
+let calculate input =
+    0
+
+[<Fact>]
+let ``Gutter game scores zero`` ()=
+    "--------------------"
+    |> calculate
+    |> should equal 0
